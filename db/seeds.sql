5e7545902c2f7a0b077ad33e22cb94b95d0f8060
kalina <kalina.boksa@gmail.com>
2022-10-11 21:29:18 -0500

    seed sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -20,3 +20,13 @@ CREATE TABLE role (
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
+
+--employee
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id)
+);
