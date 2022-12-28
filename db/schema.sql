d3636f8a01e6994aff4995198f4652b95b2197d0
kalina <kalina.boksa@gmail.com>
2022-12-27 21:08:26 -0600

    trying to fix error

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,24 @@
--- Active: 1664832326234@@127.0.0.1@3306@user_db
+-- Active: 1672195907899@@127.0.0.1@3306@emptrack_db
 -- As the image illustrates, your schema should contain the following three tables:
 DROP DATABASE IF EXISTS emptrack_db;
 CREATE DATABASE emptrack_db;
 
 USE emptrack_db;
 
---department
+-- department
+    -- id: INT PRIMARY KEY
+    -- name: VARCHAR(30) to hold department name
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
+-- role
+    -- id: INT PRIMARY KEY
+    -- title: VARCHAR(30) to hold role title
+    -- salary: DECIMAL to hold role salary
+    -- department_id: INT to hold reference to department role belongs to
 
---role
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
@@ -20,8 +26,13 @@ CREATE TABLE role (
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
+-- employee
+    -- id: INT PRIMARY KEY
+    -- first_name: VARCHAR(30) to hold employee first name
+    -- last_name: VARCHAR(30) to hold employee last name
+    -- role_id: INT to hold reference to employee role
+    -- manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
 
---employee
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
